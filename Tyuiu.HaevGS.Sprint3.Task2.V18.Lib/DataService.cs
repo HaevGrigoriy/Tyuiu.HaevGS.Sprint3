84d8296cccb9904d9f271438d437a1e0b107ec7b
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-18 22:15:24 +0500

    Task 2

---
+++ Tyuiu.HaevGS.Sprint3.Task2.V18.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.HaevGS.Sprint3.Task2.V18.Lib
+{
+    public class DataService : ISprint3Task2V18
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            int sm = 0;
+            do
+            {
+                sm += (int)(Math.Cos(value) + Math.Pow(startValue / 8, 3));
+                startValue++;
+            } while (startValue <= stopValue);
+            return sm;
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint3.Task2.V18/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.HaevGS.Sprint3.Task2.V18.Lib;
+
+DataService ds = new DataService();
+
+int value = 1;
+int startValue = 1;
+int stopValue = 14;
+
+Console.WriteLine(ds.GetMultiplySeries(value, startValue, stopValue));
+Console.ReadKey();
