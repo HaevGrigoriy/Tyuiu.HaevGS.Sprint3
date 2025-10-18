ae93a24c9a5236727637994673fb37467a7558c0
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-18 21:41:07 +0500

    Task 1

--- Tyuiu.HaevGS.Sprint3.Task1.V16.Lib/DataService.cs
+++ Tyuiu.HaevGS.Sprint3.Task1.V16.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.HaevGS.Sprint3.Task1.V16.Lib
     {
         public double GetSumSeries(double value, int startValue, int stopValue)
         {
-            double ms = 1;
+            double ms = 0;
             while(startValue <= stopValue)
             {
                 ms += Math.Pow(value, 2) * Math.Sin(ms) + 1;
