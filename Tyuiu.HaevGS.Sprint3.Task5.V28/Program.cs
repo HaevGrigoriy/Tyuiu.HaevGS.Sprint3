ab43e04ae97f7ed63f7dbb9fa1b04cce805d8441
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-19 19:17:39 +0500

    Task 5

--- Tyuiu.HaevGS.Sprint3.Task5.V28/Program.cs
+++ Tyuiu.HaevGS.Sprint3.Task5.V28/Program.cs
@@ -8,5 +8,5 @@ int startValue2 = 1;
 int stopValue1 = 3;
 int stopValue2 = 12;
 
-Console.WriteLine(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+Console.WriteLine(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
 Console.ReadKey();
