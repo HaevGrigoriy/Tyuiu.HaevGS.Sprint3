d4d8b9f824f20c51682e7de684ba3c96cb34439a
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-19 13:27:48 +0500

    Task 4

---
+++ Tyuiu.HaevGS.Sprint3.Task4.V13.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.HaevGS.Sprint3.Task4.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint3.Task4.V13/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.HaevGS.Sprint3.Task4.V13;
+using Tyuiu.HaevGS.Sprint3.Task4.V13.Lib;
+
+DataService ds = new DataService();
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine(ds.Calculate(startValue, stopValue));
+Console.ReadKey();
