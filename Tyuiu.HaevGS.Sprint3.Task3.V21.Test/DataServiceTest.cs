af5e50e0311380e4115005796c29011f868f759b
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-19 12:48:11 +0500

    Task 3

---
+++ Tyuiu.HaevGS.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.HaevGS.Sprint3.Task3.V21;
+
+namespace Tyuiu.HaevGS.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.HaevGS.Sprint3.Task3.V21.Lib;
+
+DataService ds = new DataService();
+
+string value = "f3g5ht g4j 34kg4";
+char item = 'e';
+
+Console.WriteLine(ds.ReplaceNumOnChar(value, item));
+Console.ReadKey();
