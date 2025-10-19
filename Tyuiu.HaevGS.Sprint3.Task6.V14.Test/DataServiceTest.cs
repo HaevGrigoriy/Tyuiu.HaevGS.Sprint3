99eab169dfa25ebeb80b64bce44be0f9bb92eb54
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-10-19 20:46:29 +0500

    Task 6

---
+++ Tyuiu.HaevGS.Sprint3.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.HaevGS.Sprint3.Task6.V14.Lib;
+
+namespace Tyuiu.HaevGS.Sprint3.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint3.Task6.V14/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.HaevGS.Sprint3.Task6.V14.Lib;
+
+DataService ds = new DataService();
+
+
+int startValue = 7;
+int stopValue = 16;
+Console.WriteLine(ds.GetSumTheDivisors(startValue, stopValue));
+Console.ReadKey();
